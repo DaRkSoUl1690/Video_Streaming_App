cd3b35e9021db1441bca8fd6668dade4ab7846e9
DaRkSoUl1690 <tiwarivedant.1690@gmail.com>
2022-06-13 01:11:02 +0530

    fourth commit added logout functionality

--- app/src/main/java/com/example/video_streaming_app/Dashboard.java
+++ app/src/main/java/com/example/video_streaming_app/Dashboard.java
@@ -1,14 +1,17 @@
 package com.example.video_streaming_app;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.video_streaming_app.databinding.ActivityDashboardBinding;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
@@ -26,6 +29,7 @@ public class Dashboard extends AppCompatActivity {
     ActivityDashboardBinding binding;
     DatabaseReference likeReference;
     boolean testClick;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,9 +37,9 @@ public class Dashboard extends AppCompatActivity {
         setContentView(binding.getRoot());
         likeReference = FirebaseDatabase.getInstance().getReference("likes");
 
-       binding.addVideo.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(),addvideo.class)));
+        binding.addVideo.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), addvideo.class)));
 
-       binding.reView.setLayoutManager(new LinearLayoutManager(this));
+        binding.reView.setLayoutManager(new LinearLayoutManager(this));
 
         FirebaseRecyclerOptions<Model> options =
                 new FirebaseRecyclerOptions.Builder<Model>()
@@ -43,17 +47,17 @@ public class Dashboard extends AppCompatActivity {
                         .build();
 
 
-        FirebaseRecyclerAdapter<Model,myViewHolder> adapter = new FirebaseRecyclerAdapter<Model, myViewHolder>(options) {
+        FirebaseRecyclerAdapter<Model, myViewHolder> adapter = new FirebaseRecyclerAdapter<Model, myViewHolder>(options) {
             @Override
             protected void onBindViewHolder(@NonNull myViewHolder holder, int position, @NonNull Model model) {
-               holder.prepareExoPlayer(getApplication(), model.getTitle(), model.getVurl());
+                holder.prepareExoPlayer(getApplication(), model.getTitle(), model.getVurl());
 
-               FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+                FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
                 assert firebaseUser != null;
                 String userId = firebaseUser.getUid();
                 String postKey = getRef(position).getKey();
 
-                holder.getLikeButtonStatus(postKey,userId);
+                holder.getLikeButtonStatus(postKey, userId);
 
                 holder.imageView.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -62,18 +66,15 @@ public class Dashboard extends AppCompatActivity {
                         likeReference.addValueEventListener(new ValueEventListener() {
                             @Override
                             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                              if(testClick)
-                              {
-                                  if(snapshot.child(postKey).hasChild(userId))
-                                  {
-                                      likeReference.child(postKey).removeValue();
-                                      testClick = false;
-                                  }
-                                  else {
-                                      likeReference.child(postKey).child(userId).setValue(true);
-                                      testClick = false;
-                                  }
-                              }
+                                if (testClick) {
+                                    if (snapshot.child(postKey).hasChild(userId)) {
+                                        likeReference.child(postKey).removeValue();
+                                        testClick = false;
+                                    } else {
+                                        likeReference.child(postKey).child(userId).setValue(true);
+                                        testClick = false;
+                                    }
+                                }
 
                             }
 
@@ -90,11 +91,31 @@ public class Dashboard extends AppCompatActivity {
             @NonNull
             @Override
             public myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-               View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.singlerow,parent,false);
-                   return new myViewHolder(view);
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.singlerow, parent, false);
+                return new myViewHolder(view);
             }
         };
         adapter.startListening();
         binding.reView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.appmenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.logout) {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
+        if (item.getItemId() == R.id.manage_profile) {
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
